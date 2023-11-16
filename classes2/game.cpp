b6ca30176bb87ea96c2ca2747795038586cd4a8a
mmeka24 <85970404+mmeka24@users.noreply.github.com>
2023-11-15 22:01:27 -0800

    Update game.cpp

--- classes2/game.cpp
+++ classes2/game.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 game::game() : movie::movie(){
 		cout << "Enter the fields in the following format with commas: title,year,publisher,rating. Without spaces!"<<endl;
+		//using strtok to split the responses and store them as fields 
 		cin.getline(response,150);
 		strcpy(title,strtok(response,","));
 		strcpy(year, strtok(NULL,","));
