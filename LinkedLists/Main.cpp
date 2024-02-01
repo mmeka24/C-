918bb3c348ae150820a20ab2cfe1e7e427624665
U-SRIMEKA\mekas <mekasvi@gmail.com>
2024-01-31 20:57:39 -0800

    first

--- LinkedLists/Main.cpp
+++ LinkedLists/Main.cpp
@@ -9,100 +9,31 @@
 
 using namespace std;
 
-void add(Node* cur, Node* prev, Node* &head);
-void print(Node* next);
-float getAvg(Node* current, int total);
-void remove(Node* &head, Node* current, Node* prev,  int studentID, int &total);
 
-
-
-void add(Node* newNode, Node* cur, Node* prev, Node* &head) {
-  if (cur == NULL) {
-    //checking if the current node is null 
-    if (prev == NULL) {
-      //checking if the previous node is null so llist is empty
-      //if empty then the newNode will be HEAD
-      head = newNode;
-    } else{
-      //if current node is null than prev updated to point to new node  
-      prev->setNext(newNode);
-    }
-    return;
-  }
-  //organizes the ids
-  if(newNode->getStudent()->getStudentID() < cur->getStudent()->getStudentID()) {
-    // if the new node has a smaller student ID, insert it before the current node
-    newNode->setNext(cur);
-    if (prev != NULL) {
-      //if previous node 
-      prev->setNext(newNode);
-    } else {
-      //no previous nodes at all newNode is head again 
-      head = newNode;
-    }
-  } else {
-    add(newNode, cur->getNext(), cur, head);
-  }
-}
-
-
-float getAvg(Node* current, int total){
-  if (current == NULL) {
-    return 0;
-  }
-  //incremented total in the main 
-  return current->getStudent()->getGPA()/total + getAvg(current->getNext(), total);
-
-}
-
-
-void print(Node* next){
-  if (next == NULL) {
-    return;
-    //if next node has nothing then don't print 
-  }
-  Student* cur = next->getStudent(); //short hand version to deference addresses
-  cout << "student id: " << cur->getStudentID() << endl;
-  cout << "first name: "<< cur->getFirstName() << endl; 
-  cout << "last name: " << cur->getLastName() << endl; 
-  cout << "gpa: "<< fixed << setprecision(2) << cur->getGPA() << endl;
-  print(next->getNext());
-}
-
-
-void remove(Node* &head, Node* current, Node* prev, int studentID, int &total) {
-  if (current == NULL) {
-    return; //nothing in. skip. 
-  }
-  if (current->getStudent()->getStudentID() == studentID) {
-    if (prev != NULL) {
-      //prev->getNext()->setNext(current->getNext());
-      prev->setNext(current->getNext()); //changed this LINE
-    } 
-    else {
-      head = head->getNext();
-    }
-    total--;
-
-    delete current;
-  } else {
-    remove(head, current->getNext(), current, studentID, total);
-  }
-}
-
+void rehash(int &bucketSize, Node** &hashTable);
 
 
 int main() {
-  int total = 0;
   char inp[20];
   char first[20];
   char last[20];
   char input[10]; 
   int id; 
-  Node* head = NULL; 
+  
+
+    srand(time(NULL));
+  int initialBucket = 100;
+  int bucketSize = initialBucket;
+  Node** hashTable = new Node*[bucketSize];
+
+  for (int i = 0; i < bucketSize; i++) {
+    hashTable[i] = NULL;
+  }
+
+
   bool running = true; 
   while(running){
-    cout << "what would you like to do: add, print, delete, or avg? " << endl; 
+    cout << "what would you like to do: add, print, delete? " << endl; 
     cin >> inp; 
     if(strcmp(inp, "add") == 0){
       
@@ -131,26 +62,86 @@ int main() {
       cin >> GPA;
       Student* student = new Student(first, last, studentID, GPA);
       add(new Node(student), head, NULL, head);
-      total += 1;
+      int hashIndex = newStudent->get_id() % bucketSize;
+
+       if(hashTable[hashIndex] == NULL){
+          hashTable[hashIndex] = new Node; 
+      }
+
+      //if there is already something in the hash table then create a new node
+      if(hashTable[hashIndex] != NULL){
+        Node* temp = hashTable[hashIndex];  
+      if(temp->getNext() != NULL){
+            temp = temp->getNext(); 
+        }
+        temp->getNext(new Node);  
+
+      else{
+        cout << "something is wrong"; 
+      }
+
+      }
 
     }
     //print
     else if(strcmp(inp,"print") == 0) {
-                print(head);
+       hashTable.print();
+    for (int i = 0; i < bucketSize; i++){
+      if(hashTable[i] != NULL){
+        Node *temp = hashTable[i]; 
+
+        while (temp != NULL){
+        cout << temp->getStudent->get_first_name << temp->getStudent->get_last_name << temp->getStudent->get_id << temp->getStudent->get_gpa; 
+        temp = temp->getNext(); 
+      }
+
+      }
     }
-    //avg 
-    else if(strcmp(inp,"avg") == 0) {
-		cout << "The average gpa is: " << getAvg(head, total) << endl;
-		}
+    }
+    
 
 //deleting
 else if(strcmp(inp,"delete") == 0) { 
 			cout << "Enter the id of the student you would like to delete" << endl;
 			int studentID; 
       cin >> studentID;
-			Node* current = head;
-			remove(head, current, NULL , studentID, total);
-      
+		  hashTable.remove(studentID);
+
+ int hashIndex = studentID % bucketSize;
+
+ for (int i = 0; i < bucketSize; i++){
+  int hashIndex = studentID % bucketSize; 
+
+      if(hashTable[hashIndex] != NULL){
+        Node *temp = hashTable[i]; 
+        Node *prev = NULL; //using prev to update after deletion 
+
+        while(temp!=NULL){
+          if(temp->getStudent->get_id == studentID){
+            if(prev = NULL){
+              //if prev is NULL then move the temp next pointer to now be the head
+              hashTable[hashIndex] = temp->getNext(); 
+              delete temp; 
+            }
+            else{
+              prev->getNext() = temp->getNext(); 
+              delete temp; 
+              temp = prev; 
+            }
+
+          }
+        
+          prev = temp; //prev = temp is same as prev = prev->getNext(); 
+          temp = temp->getNext(); 
+
+        }
+
+      }
+
+      }
+    }
+
+
 
 		} 
 
@@ -158,11 +149,14 @@ else if(strcmp(inp,"delete") == 0) {
     else if (strcmp(input, "QUIT") == 0) {
       running = false;
     }
+
+    return 0; 
   }
 
-  
+
+
+void rehash(int &bucketSize, Node** &hashTable){
+  bucketSize = bucketSize * 2; 
 
 
 }
-
-
--- classes2/movie.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include "music.h"
-using namespace std;
-class movie: public music{//inherit from music because fields are similar
-	public:
-		movie();//same constructor for all
-		char * get_rating();
-};
